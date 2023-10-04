250561016541e7a0b7027783c31bc888e2fa9f0d
Dario pm <a01785420@tec.mx>
2023-10-04 10:55:04 -0600

    1.03

--- python/tareas/tarea4/programa4.py
+++ python/tareas/tarea4/programa4.py
@@ -1,5 +1,11 @@
 numeroSerie= int(input("ingrese numero de elementos"))
 resultado = 1
+resultadoN= 1
+print("n =", resultado, "= 1/",resultado, "=", 1)
 for i in range(1,numeroSerie):
+    
+    resultadoN= 1 / (i+1 )
+    print("n =", i, "= 1/",i+1, "=", round(resultadoN, 2))
+    
     resultado = resultado + (1 / (i+1 ))
-print (resultado)    
+print("El resultado de la suma de la secuencia es =", round(resultado, 2))    
